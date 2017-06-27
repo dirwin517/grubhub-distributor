39bf310509c588cf0ab15df9c0726c3a5f837a8a
Daniel Irwin <daniel.irwin@enernoc.com>
2017-06-27 11:36:51 -0400

    move grub into bin

--- bin/grub.js
+++ bin/grub.js
@@ -2,11 +2,11 @@
  * Created by daniel.irwin on 4/5/17.
  */
 'use strict';
-let GrubHub = require('./lib/grubhubclient');
+let GrubHub = require('../lib/grubhubclient');
 
 let client = GrubHub;
 
-let Calc = require('./lib/calc');
+let Calc = require('../lib/calc');
 
 let calculate = Calc;
 
